29a13f6900efb6cbf282263df17e0f39ca784641
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-25 12:24:02 +0530

    Update terraform/tf-network/

---
+++ outputs.tf
@@ -0,0 +1,40 @@
+output "VPC_ID" {
+  value = module.network.VPC_ID
+}
+
+output "VPC_CIDR" {
+  value = module.network.VPC_CIDR
+}
+
+output "PUBLIC_SUBNETS_IDS" {
+  value = module.network.PUBLIC_SUBNETS_IDS
+}
+
+output "PUBLIC_SUBNETS_CIDR" {
+  value = module.network.PUBLIC_SUBNETS_CIDR
+}
+
+output "PRIVATE_SUBNETS_IDS" {
+  value = module.network.PRIVATE_SUBNETS_IDS
+}
+
+output "PRIVATE_SUBNETS_CIDR" {
+  value = module.network.PRIVATE_SUBNETS_CIDR
+}
+
+output "DEFAULT_VPC_ID" {
+  value = module.network.DEFAULT_VPC_ID
+}
+
+output "DEFAULT_VPC_CIDR" {
+  value = module.network.DEFAULT_VPC_CIDR
+}
+
+output "INTERNAL_HOSTED_ZONEID" {
+  value = module.network.INTERNAL_HOSTED_ZONEID
+}
+
+output "INTERNAL_HOSTED_ZONE_NAME" {
+  value = module.network.INTERNAL_HOSTED_ZONE_NAME
+}
+
